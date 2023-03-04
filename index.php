d16015c59dd03c95910c7695b6167e637c4de3dc
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2023-03-04 14:44:33 -0400

    Criar o navbar do sistema

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'src/php/components/navbar.php'
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,25 +10,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>petful</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-        body {
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        h1 {
-            color: gold;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>petful</h1>
+    <?php navbar();?>
 </body>
 </html>
